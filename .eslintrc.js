cdb0c3341a01ae9c0acefe31817c561ab93246e8
Ermal Kaleci <ermalkaleci@gmail.com>
2023-04-27 15:54:58 +0200

    updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
   ],
   parserOptions: {
     ...base.parserOptions,
-    project: ['./tsconfig.eslint.json']
+    project: ['./tsconfig.json']
   },
   rules: {
     ...base.rules,
